e2d02a9003ca771003808ae7b0d0e07fc26d23d3
Jimmy Dave <jimmydave03@yahoo.co.in>
2021-10-24 00:18:32 +0530

    Added component and divide app into layout

---
+++ src/component/footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Footer.css';
+
+class Footer extends React.Component {
+    render() : React.ReactElement {
+        return(
+            <div>
+                <h1>This is a footer</h1>
+            </div>
+        );
+    }
+}
+
+export default Footer;
